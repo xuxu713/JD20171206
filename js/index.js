ce4a8a418fa851b9adf8ba693ed1766174fa8437
190159372@qq.com <190159372@qq.com>
2017-12-08 22:54:39 +0800

    20171208

--- js/index.js
+++ js/index.js
@@ -28,7 +28,7 @@
 	sktimer = setInterval(rightClick, 2000);
 }
 new SecKill();*/
-function SecKill(box) {
+function banner(box) {
 	$box = $(box);
 	$imgs = $(box).find('ul').children('li');
 	$btns = $(box).find('ol').children('li');
@@ -57,4 +57,74 @@ function SecKill(box) {
 	}
 	timer = setInterval(rightClick, 2000);
 }
-new SecKill('.sk-special');
+banner('.sk-special'); //京东秒杀模块，最右侧轮播效果
+
+//实现顶部悬浮搜索框
+$(window).scroll(function() {
+	var $scrolltop = $(document).scrollTop();
+	var $fixhead = $('#fixhead');
+	if($scrolltop >= 300) {
+		$fixhead.show();
+		$fixhead.stop().animate({
+			height: '48px'
+		}, 200);
+	} else {
+		$fixhead.height('0');
+		$fixhead.hide();
+
+	}
+});
+
+function myjd() {
+	var $myjd = $('.myjd');
+	var $myjdli = $('.myjdli');
+	var $submyjd = $('.submyjd');
+	$myjdli.hover(function() {
+		$myjdli.css({
+			'background': 'white',
+		});
+		$myjd.css({
+			'color': '#c81623'
+		})
+		$submyjd.stop().delay(100).show();
+	}, function() {
+			$myjdli.css({
+				'background': '#e3e4e5',
+			});
+			$myjd.css({
+				'color': '#999'
+			})
+			$submyjd.stop().delay(500).hide();
+	})
+}
+myjd();
+
+/*function stayleave() {
+	var $myjdli = $('.myjdli');
+	var $submyjd = $('.submyjd');
+	$myjdli.hover(function() {
+		$myjdli.stop().show();
+	}, function() {
+		$submyjd.stop().hide();
+	})
+}*/
+function stayleave(obj='.myjdli',subobj='.submyjd') {
+	var $obj= $(obj);
+	var $subobj = $(subobj);
+	$obj.hover(function() {
+		$obj.stop().show();
+	}, function() {
+		$subobj.stop().hide();
+	})
+}
+stayleave('.myjdli','.submyjd');
+stayleave('.serviceli','.subservice');
+function hoverColor(obj='.submyjd',ele='a',colorafter='#c81623',colorbefore='#999'){
+	var $ele=$(obj).find(ele);
+	$ele.hover(function(){
+		$(this).css('color',colorafter)
+	},function(){
+		$(this).css('color',colorbefore);
+	})
+}
+hoverColor();
