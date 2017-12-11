4d417c5bc28ec686583be6beb6178dbf7972e67b
xuxu713 <190159372@qq.com>
2017-12-11 21:17:13 +0800

    Signed-off-by: xuxu713 <190159372@qq.com>

---
+++ js/details.js
@@ -0,0 +1,101 @@
+function hoverColor(obj, ele, colorafter = '#c81623', colorbefore = '#999', weight = 'normal', attr = 'color') { //划过盒子，指定子元素变色
+	var $ele = $(obj).find(ele);
+	$ele.hover(function() {
+		$(this).css(attr, colorafter);
+		$(this).css('font-weight', weight)
+	}, function() {
+		$(this).css(attr, colorbefore);
+		$(this).css('font-weight', 'normal')
+	})
+}
+hoverColor('.navitems', 'a', '#b61d1d', '#666'); //details.html
+function magnifier() {
+	var $spic = $('#spic');
+	var $sf = $('#sf');
+	var $bf = $('#bf');
+	var $bpic = $('#bpic');
+	var $left = $('#left');
+	var $right = $('#right');
+	var $num = 6;
+	var $list = $('#list li');
+	var $ul = $('#list ul');
+	$spic.hover(function(ev) {
+		$sf.show();
+		$bf.show();
+		$bpic.show();
+		$sf.width($bf.width() * $spic.width() / $bpic.width());
+		$sf.height($bf.height() * $spic.height() / $bpic.height());
+		$(document).on('mousemove', function(ev) {
+			var $l = ev.clientX - $spic.offset().left - ($sf.width() / 2);
+			var $t = ev.clientY - $spic.offset().top - ($sf.height() / 2);
+			var $scale = $bpic.width() / $spic.width();
+			if($l <= 0) {
+				$l = 0;
+			}
+			if($l >= $spic.width() - $sf.width()) {
+				$l = $spic.width() - $sf.width();
+			}
+			if($t <= 0) {
+				$t = 0;
+			}
+			if($t >= $spic.height() - $sf.height()) {
+				$t = $spic.height() - $sf.height();
+			}
+			$sf.css({
+				left: $l,
+				top: $t
+			});
+			$bpic.css({
+				left: -$scale * $l,
+				top: -$scale * $t
+			})
+		});
+	}, function() {
+		$sf.hide();
+		$bf.hide();
+		$bpic.hide();
+	});
+	if($num >= $list.length) {
+		$right.css({
+			color: '#fff'
+		})
+	}
+	$right.on('click', function() {
+		if($num < $list.length) {
+			$num++;
+			if($num == $list.length) {
+				$right.css({
+					color: '#fff'
+				});
+			}
+			$left.css({
+				color: '#333'
+			})
+		}
+		$ul.animate({
+			left: -$list.innerWidth() * ($num - 6)
+		}, 100);
+	});
+	$left.on('click', function() {
+		if($num > 6) {
+			$num--;
+			if($num == 6) {
+				$left.css({
+					color: '#fff'
+				});
+			}
+			$right.css({
+				color: '#333'
+			});
+		}
+		$ul.animate({
+			left: -$list.innerWidth() * ($num - 6)
+		}, 100)
+	});
+	$list.on('click', function() {
+		var $src = $(this).children('img').attr('src');
+		$('#spic img').attr('src', $src);
+		$('#bpic').attr('src', $src);
+	});
+}
+magnifier();
--- php/test.php
+++ php/test.php
@@ -1,11 +1,11 @@
 <?php
-	$obj = $_POST["obj"];
-	$ele = $_POST["ele"];
+	$obj = $_REQUEST['obj'];
+	$ele = $_REQUEST['ele'];
 
-	mysql_connect("localhost:3300","root","123456");
+	mysql_connect("localhost:3306","root","12345678");
     mysql_select_db("xuyuanjd");
-    mysql_query("SET NAMES UTF-8");
-    $query = "select result from test where obj = '{$obj}' and ele = '{$ele}' order by ordernum";
+    mysql_query("SET NAMES UTF8");
+    $query = "select result from jd1 where obj = '{$obj}' and ele = '{$ele}' order by ordernum";
 	$result = mysql_query($query); 
 	$arr=array();
 	while($row = mysql_fetch_array($result, MYSQL_ASSOC)){
