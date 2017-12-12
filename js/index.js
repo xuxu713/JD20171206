1f10e398a8b25c781bf9ea62e5f2993ce093a165
190159372@qq.com <190159372@qq.com>
2017-12-13 02:29:21 +0800

    Signed-off-by: 190159372@qq.com <190159372@qq.com>

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-function phpMysql(obj, ele) {
+/*function phpMysql(obj, ele) {
 	$.post('php/test.php', {
 		obj: obj,
 		ele: ele
@@ -15,10 +15,10 @@ function phpMysql(obj, ele) {
 		hoverColor('.spesubweb', 'a'); //index.html>shortcut>nav-li>二级菜单，a字体变色
 		hoverColor('.subdrop', 'a');
 	})
-}
-phpMysql('.submyjd .list-top', 'a');
-phpMysql('.submyjd .list-bottom', 'a');
-phpMysql('.subdrop .subdropol', 'a');
+}*/
+//phpMysql('.submyjd .list-top', 'a');
+//phpMysql('.submyjd .list-bottom', 'a');
+//phpMysql('.subdrop .subdropol', 'a');
 
 function banner(box) { //轮播图效果
 	var $box = $(box);
@@ -156,7 +156,8 @@ hoverColor('.ask', 'a'); //划过login.html-header-login-ask，a字体变色#e43
 hoverColor('.news-head', '.news-more', '#c81623', '#666');
 hoverColor('.cate_menu', '.cate_menu_item', '#999395', '#6e6568', 'normal', 'background-color');
 hoverColor('.cate_menu', '.cate_menu_lk', '#c81623', '#fff');
-
+hoverColor('.user-info-show', 'a', '#db192b', '#666');
+hoverColor('#shortcut', '.style-red', '#db192b', '#f10215'); //顶部shortcut里class设置为style-red的标签，鼠标划出后颜色为原设置红色
 
 function hoverChangeBgPosition(ele, attr, aftervalue, beforevalue) { //划过div，改变背景图片位置来改变显示
 	$(ele).hover(function() {
@@ -182,3 +183,93 @@ function lineChangePosition() { //.line根据tab选项卡的改变定位left值
 
 }
 lineChangePosition();
+
+function step() {
+	$(window).on('scroll', function() { //滚动条距离控制楼梯的显示隐藏
+		var $top = $(this).scrollTop();
+		var $maxheight = $('#headerlt').height() / 2; //设置楼梯出现的参考值
+		if($top >= ($maxheight + 100)) {
+			$('#loutinav').show();
+		} else {
+			$('#loutinav').hide();
+		}
+
+		$('.louti').each(function() { //拖动滚轮对应的楼梯添加active类
+			var $current = $('.louti').eq($(this).index('.louti'));
+			var $loutitop = $current.offset().top + $current.height() / 2; //得到当前滚轮的距离+当前div的高度/2
+			if($loutitop > $top) {
+				$('#loutinav li').removeClass('active');
+				$('#loutinav li').eq($(this).index()).addClass('active');
+				return false;
+			}
+		});
+	});
+	$('#loutinav li').not('.last').on('click', function() { //点击当前楼梯按钮，给楼梯移除和添加active。通过index的一致，控制滚轮距离跳转到具体的div
+		$(this).addClass('active').siblings('li').removeClass('active');
+		var $offsettop = $('.louti').eq($(this).index('li')).offset().top;
+		$('html,body').animate({
+			scrollTop: $offsettop
+		});
+	});
+	$('#loutinav li').not('.last').hover(function() { //滑过楼梯添加active类，滑出取消。(冲突-拖动滚轮对应的楼梯添加active类，未调好)
+		$(this).addClass('active');
+	}, function() {
+		$(this).removeClass('active');
+		var $offsettop = $('.louti').eq($(this).index('li')).offset().top;
+		console.log($offsettop);
+		console.log($(window).scrollTop());
+		if($offsettop == $(window).scrollTop()) {
+			$('#loutinav li').eq($(this).index('li')).addClass('active');
+		}
+	});
+	$('.last').on('click', function() { //回到顶部
+		$('html,body').animate({
+			scrollTop: 0
+		});
+	});
+};
+step();
+
+function shangcheng() { //商城效果，左右移动来更换图片
+	var $box = $('.box_bd .sk-list');
+	var $imgs = $(box).find('.sk-item');
+	var $liLength = $imgs.width();
+	var $btns = $(box).find('ol li');
+	var $left = $(box).find('.sk_controls_prev');
+	var $right = $(box).find('.sk_controls_next');
+
+	var timer = null;
+	var $index = 0;
+
+	$btns.on('mouseover', function() {
+		$index = $(this).index();
+		tabSwitch();
+	});
+
+	function tabSwitch() {
+		$btns.eq($index).addClass('active').siblings($btns).removeClass('active');
+		$imgs.eq($index).siblings().fadeOut();
+		$imgs.eq($index).fadeIn();
+	}
+
+	$box.hover(function() {
+		clearInterval(timer);
+		$left.show();
+		$right.show();
+	}, function() {
+		timer = setInterval(rightClick, 2000);
+		$left.hide();
+		$right.hide();
+	});
+
+	function rightClick() {
+		$index < ($btns.length - 1) ? $index++ : $index = 0;
+		tabSwitch();
+	}
+	$right.on('click', rightClick);
+	$left.on('click', function() {
+		$index > 0 ? $index-- : $index = ($btns.length - 1);
+		tabSwitch();
+	});
+	timer = setInterval(rightClick, 2000);
+}
